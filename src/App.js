654b2d2585e7dd80ddb2a36b20be25fc85c03588
Kapil Shah <kapil.shah@artoon.in>
2024-11-15 11:38:25 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Welcome to your first React app!</p>
+    </div>
+  );
+}
+
+export default App;
+
